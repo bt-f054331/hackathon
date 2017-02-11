77df70ab66a50e174ac737369d70103a7d6e01d4
bt-f054331 <dorian.bates@btfinancialgroup.com>
2017-02-12 10:10:21 +1100

    put voice back

--- js/app/app-controllers.js
+++ js/app/app-controllers.js
@@ -38,7 +38,6 @@ function overviewController ( $scope, $rootScope, wpDataService, wpUtilService, 
     var self = this;
     self.items = ['super'],['loan'];
 
-
     self.execute = function(val, keys) {
       var target = wpUtilService.matcher(val, keys);
       self.navigate(target);
@@ -55,5 +54,14 @@ function overviewController ( $scope, $rootScope, wpDataService, wpUtilService, 
         }
     }
 
+    var commands = {
+      '*val' : function(val) {
+        self.execute(val, self.items);
+      }
+    }
+
+    annyang.addCommands(commands);
+    annyang.debug();
+    annyang.start();
 
 }
